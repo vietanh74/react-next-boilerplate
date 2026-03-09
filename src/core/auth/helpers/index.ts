7d28e86ffb06cf06195d16d37f7247db56fdbbc4
anhtv3 <anhtv3@crownx.masangroup.com>
2026-03-09 14:09:12 +0700

    feat: upgrade

--- src/core/auth/helpers/index.ts
+++ src/core/auth/helpers/index.ts
@@ -1,10 +1,3 @@
-import { get } from 'lodash';
-import { includes } from 'lodash';
-
-import { jsonDecode, jsonEncode } from '@/helpers';
-import { IdStorageName, ID_STORAGE_ALL } from '@/shared/constants';
-import { oidc } from '@/shared/constants';
-
 export const APP_ACCESS_TOKEN = 'app_access_token';
 export const APP_REFRESH_TOKEN = 'app_refresh_token';
 
@@ -21,44 +14,8 @@ export const authGuard = async ({
 };
 
 export const authLogout = async (): Promise<void> => {
-  spreadIdStorage();
-  const primaryAuth = new window['gmjs'].primaryAuth(oidc);
-
-  try {
-    await primaryAuth.signOut({
-      revokeAccessToken: true,
-      postLogoutRedirectUri: `${oidc.authnUrl}/sign-out?continue=${window.location.origin}`,
-    });
-  } catch (e) {
-    window.location.href = `${oidc.authnUrl}/sign-out?continue=${window.location.origin}`;
-  } finally {
-    localStorage.clear();
-  }
-};
-
-export const getIdAccessToken = (): string | undefined => {
-  const tokenStorage = jsonDecode(localStorage.getItem(IdStorageName.PrimaryTokenStorage));
-
-  return get(tokenStorage, 'accessToken.accessToken');
-};
-
-export const moveIdStorage = (): void => {
-  const allStorage = {};
-
-  Object.keys(IdStorageName).forEach((key) => {
-    allStorage[key] = jsonDecode(localStorage.getItem(IdStorageName[key]));
-    localStorage.removeItem(IdStorageName[key]);
-  });
-
-  localStorage.setItem(ID_STORAGE_ALL, jsonEncode(allStorage) || '{}');
-};
-
-export const spreadIdStorage = (): void => {
-  const allStorage = jsonDecode(localStorage.getItem(ID_STORAGE_ALL));
-
-  Object.keys(allStorage).forEach((key) => {
-    localStorage.setItem(IdStorageName[key], jsonEncode(allStorage[key]) || '');
-  });
+  localStorage.clear();
+  //
 };
 
 export const getAppAccessToken = (): string | null => {
@@ -84,36 +41,4 @@ export const setAppToken = (authInfo: any): void => {
 export const removeAppToken = (): void => {
   localStorage.removeItem(APP_ACCESS_TOKEN);
   localStorage.removeItem(APP_REFRESH_TOKEN);
-};
-
-export const canUpdate = (scopes: string[], role: string) => {
-  if (includes(scopes, role) || includes(scopes, `${role}.update`)) {
-    return true;
-  }
-
-  return false;
-};
-
-export const onlyView = (scopes: string[], role: string) => {
-  if (includes(scopes, role) || includes(scopes, `${role}.update`)) return false;
-
-  return includes(scopes, `${role}.view`);
-};
-
-export const viewLimit = (scopes: string[], role: string) => {
-  if (includes(scopes, `${role}.update`) || includes(scopes, `${role}.view`)) return false;
-
-  return true;
-};
-
-export const getAccessTokenPrimary = (): string | undefined => {
-  const tokenStorage = jsonDecode(localStorage.getItem(ID_STORAGE_ALL));
-
-  return get(tokenStorage, 'PrimaryTokenStorage.accessToken.accessToken');
-};
-
-export const getIdExpiredTime = (): number => {
-  const tokenStorage = jsonDecode(localStorage.getItem(ID_STORAGE_ALL));
-
-  return get(tokenStorage, 'PrimaryTokenStorage.accessToken.expiresAt');
 };
